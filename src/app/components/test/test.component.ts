516dedb935859c82ee893d44ef37384872478cfd
ju_ni <juliandk1357@gmail.com>
2024-04-27 03:04:28 +0200

    Beispiel Bar-Plot mit Plotly Component
    Angular Plotly Komponente installiert:
    https://github.com/plotly/angular-plotly.js/
    
    und einfaches Bar-Chart erstellt

--- src/app/components/test/test.component.ts
+++ src/app/components/test/test.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
 
+import * as Plotly from 'plotly.js-dist-min';
+import { PlotlyModule } from 'angular-plotly.js';
+
+PlotlyModule.plotlyjs = Plotly;
+
 @Component({
   selector: 'app-test',
   standalone: true,
-  imports: [],
-  templateUrl: './test.component.html',
+  imports: [PlotlyModule],
+  //templateUrl: './test.component.html',
+  template: '<plotly-plot [data]="graph.data" [layout]="graph.layout"></plotly-plot>',
   styleUrl: './test.component.css'
 })
 export class TestComponent {
-
+  public graph = {
+    data: [
+        //{ x: [1, 2, 3], y: [2, 6, 3], type: 'scatter', mode: 'lines+points', marker: {color: 'lightblue'} },
+        { x: [1, 2, 3], y: [2, 5, 3], type: 'bar' , marker: {color: 'lightblue'}},
+    ],
+    layout: {width: 600, height: 400, title: 'A Fancy Plot'}
+};
+  
 }
+
