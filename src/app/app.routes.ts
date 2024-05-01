7ad053b4fd43962b951d9ccbdfa249e274fc6d8a
ju_ni <juliandk1357@gmail.com>
2024-05-01 22:18:49 +0200

    404 page hinzugefügt

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 const routeConfig: Routes = [
     {
@@ -13,6 +14,11 @@ const routeConfig: Routes = [
         path: 'portfolio',
         component: PortfolioComponent,
         title: 'Portfolio Page'
+    },
+    {
+        path: '**',
+        component: PageNotFoundComponent,
+        title: 'Not Found'
     }
 ];
 
