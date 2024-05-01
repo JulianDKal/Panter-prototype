3e1ecb527c4c1d52d046242e6a3f2fcea8a6ff93
ju_ni <juliandk1357@gmail.com>
2024-05-01 21:43:45 +0200

    klickbares bild
    Portfolio Component hinzugefügt und Bild klickbar gemacht (routed jetzt zur Porfolio Seite).

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,18 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+
 const routeConfig: Routes = [
     {
         path: '',
         component: HomeComponent,
         title: 'Home Page'
+    },
+    {
+        path: 'portfolio',
+        component: PortfolioComponent,
+        title: 'Portfolio Page'
     }
 ];
 
