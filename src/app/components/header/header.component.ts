06890b52ee32e6dc22bc96bd84b019ebd0da5157
mylinhhng <s85225@informatik.htw-dresden.de>
2024-06-16 20:13:36 +0200

    Versuch Headeer beim scrollen zu verschmälern, n´hat nicht funktioniert

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-header',
+//   standalone: true,
+//   imports: [],
+//   templateUrl: './header.component.html',
+//   styleUrl: './header.component.css'
+// })
+// export class HeaderComponent {
+
+// }
+
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
+  ngOnInit(): void {
+    // Initiale Prüfung bei der Komponenten-Initialisierung
+    this.checkScroll();
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.checkScroll();
+  }
+
+  checkScroll() {
+    const headerContainer = document.querySelector('.header-container');
+    if (window.scrollY > 50) {
+      headerContainer?.classList.add('shrink');
+    } else {
+      headerContainer?.classList.remove('shrink');
+    }
+  }
 }
+
