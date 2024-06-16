f96f1cb4715b4450f4112d6e439be8d2c32d25fe
mylinhhng <s85225@informatik.htw-dresden.de>
2024-06-16 23:55:58 +0200

    header transparent beim scrollen

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,16 +1,3 @@
-// import { Component } from '@angular/core';
-
-// @Component({
-//   selector: 'app-header',
-//   standalone: true,
-//   imports: [],
-//   templateUrl: './header.component.html',
-//   styleUrl: './header.component.css'
-// })
-// export class HeaderComponent {
-
-// }
-
 import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
@@ -24,11 +11,13 @@ export class HeaderComponent implements OnInit {
   ngOnInit(): void {
     // Initiale Prüfung bei der Komponenten-Initialisierung
     this.checkScroll();
+    this.checkScroll2();
   }
 
   @HostListener('window:scroll', [])
   onWindowScroll() {
     this.checkScroll();
+    this.checkScroll2();
   }
 
   checkScroll() {
@@ -37,6 +26,15 @@ export class HeaderComponent implements OnInit {
       headerContainer?.classList.add('shrink');
     } else {
       headerContainer?.classList.remove('shrink');
+    }
+  }
+
+  checkScroll2() {
+    const header = document.getElementById('main-header');
+    if (window.scrollY > 50) {
+      header?.classList.add('scrolled');
+    } else {
+      header?.classList.remove('scrolled');
     }
   }
 }
