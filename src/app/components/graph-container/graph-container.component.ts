b6cdbcb091ae8ea145c1d505263f303ba7e155ce
ju_ni <juliandk1357@gmail.com>
2024-06-17 19:04:52 +0200

    graph container component und save
    Aus irgendeinem grund funktioniert die Plotly Angular komponente nicht mehr, deswegen erstmal workaround

--- src/app/components/graph-container/graph-container.component.ts
+++ src/app/components/graph-container/graph-container.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Chart } from '../../Chart';
+import { ChartElementComponent } from '../chart-element/chart-element.component';
 
 @Component({
   selector: 'app-graph-container',
   standalone: true,
-  imports: [],
+  imports: [ChartElementComponent],
   templateUrl: './graph-container.component.html',
   styleUrl: './graph-container.component.css'
 })
 export class GraphContainerComponent {
+  @Input() chartForGraph!:Chart;
+
 
 }
