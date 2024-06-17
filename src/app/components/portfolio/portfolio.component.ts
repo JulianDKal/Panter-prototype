b70ec4b5c3d5df0b1b41c6842305976c77ffc5b2
Shaleen <shaleen.panschenko@htw-dresden.de>
2024-06-17 09:55:51 +0200

    Update portfolio.component.ts
    mit Hover für die Graphen

--- src/app/components/portfolio/portfolio.component.ts
+++ src/app/components/portfolio/portfolio.component.ts
@@ -11,6 +11,15 @@ import { NavbarComponent } from '../navbar/navbar.component';
 export class PortfolioComponent {
   isMenuHidden = false;
 
+
+ // Diese Variable speichert den aktuellen Zustand des Graphen
+  isGraphHovered = false;
+
+  // Diese Methode wird aufgerufen, wenn der Mauszeiger über den Graphen geht
+  hoverGraph(isHovering: boolean) {
+    this.isGraphHovered = isHovering;
+  }
+
   toggleMenu() {
     this.isMenuHidden = !this.isMenuHidden;
   }
