ec4c060d8d58d07befe6125fb9a1bd3bfa2bea58
Shaleen <shaleen.panschenko@htw-dresden.de>
2024-06-17 13:28:44 +0200

    Create Chart.ts

---
+++ src/app/Chart.ts
@@ -0,0 +1,37 @@
+export class Chart {
+    //title:string = '';
+    layout:plotLayout;
+    data:plotData[];
+
+    constructor(layout:plotLayout, data:plotData[]) {
+        this.layout = layout;
+        this.data = data;
+        console.log("Chart was created: " + this.layout.title);
+    }
+}
+
+export class ScatterChart extends Chart {
+    constructor(layout:plotLayout, data: scatterData[]){
+        super(layout, data);
+    }
+}
+
+export class PieChart extends Chart {
+    constructor(layout:plotLayout, data: pieData[]){
+        super(layout, data);
+    }
+}
+
+export class BarChart extends Chart {
+    constructor(layout:plotLayout, data: barData[]){
+        super(layout, data);
+    }
+}
+
+export class BoxPlot extends Chart {
+    constructor(layout:plotLayout, data: boxData[])
+    {
+        super(layout, data);
+    }
+}
+
