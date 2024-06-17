14ad22b2f37becf677ae8cb3833eac166f8cada3
Shaleen <shaleen.panschenko@htw-dresden.de>
2024-06-17 13:29:50 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { TestComponent } from './components/test/test.component';
 import { RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, TestComponent, RouterModule],
+  imports: [RouterOutlet, HeaderComponent, TestComponent, RouterModule, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
