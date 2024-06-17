e31ba2f35df16e20f72afc82b80f83c348fc7f86
Shaleen <shaleen.panschenko@htw-dresden.de>
2024-06-17 13:29:19 +0200

    Create chart-element.component.ts

---
+++ src/app/components/chart-element/chart-element.component.ts
@@ -0,0 +1,35 @@
+import { Component, Input, OnInit, AfterViewInit, ElementRef } from '@angular/core';
+import { Chart } from '../../Chart';
+import * as Plotly from 'plotly.js-dist-min';
+import { PlotlyModule } from 'angular-plotly.js';
+
+@Component({
+  selector: 'app-chart-element',
+  standalone: true,
+  imports: [PlotlyModule],
+  template: '<plotly-plot [data]="chart.data" [layout]="chart.layout" [config]="{displayModeBar: false}"></plotly-plot>',
+  styleUrl: './chart-element.component.css'
+})
+export class ChartElementComponent {
+  @Input() chart!:Chart;
+
+  constructor(private elRef: ElementRef){}
+  configObj:any = {
+    staticPlot: true
+  }
+
+  ngOnInit()
+  {
+    console.log("the chart element was created! " + this.chart.layout.title);
+    //console.log(this.chart.data, this.chart.layout)
+  }
+
+  ngAfterViewInit()
+  {
+    const parent = this.elRef.nativeElement.parentElement;
+    this.chart.layout.height = parent.clientHeight;
+    this.chart.layout.width = parent.clientWidth;
+    
+  }
+
+}
