984494db13fdf83067aa7e3f033c24631a161311
mylinhhng <s85225@informatik.htw-dresden.de>
2024-06-18 11:35:51 +0200

    Icons eingefügt, Design Navbar geändert

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,19 +1,25 @@
 
 
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
+  imports: [RouterModule],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
   dropdownOpen = false;
+  selectedPortfolio = 'Portfolio';
 
   toggleDropdown() {
     this.dropdownOpen = !this.dropdownOpen;
   }
+
+  selectPortfolio(portfolio: string) {
+    this.selectedPortfolio = portfolio;
+    this.dropdownOpen = false;
+  }
 }
-
-
